e1b934489b30fcae7e87a2ef81e24c01b20a03b2
Taiwo Osunrinde <55808005+osunrinde@users.noreply.github.com>
2023-11-28 10:13:30 -0700

    Update Freeport.py

--- Freeport.py
+++ Freeport.py
@@ -592,13 +592,13 @@ if authentication_status:
                     #data_plot=data.loc[data['TCU']>=0.1]
                     #data_plot = data_plot[~data_plot[Ore_Type].isin([10,50,51,52,53,54])]
                     st.subheader("Filtered Assay Data:")
-                    st.dataframe(data_plot)
+                    st.dataframe(data)
                 else:
                     data = data_C.loc[data_C[column_to_filter].str.startswith(tuple(filter_list))]
                     #data_plot=data.loc[data['TCU']>=0.1]
                     #data_plot = data_plot[~data_plot[Ore_Type].isin([10,50,51,52,53,54])]
                     st.subheader("Filtered Assay Data:")
-                    st.dataframe(data_plot)
+                    st.dataframe(data)
 
                 columns = data.columns.tolist()
                 #y_options = data.columns.tolist()
