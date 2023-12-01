1557d15e9e3bdfb3d8f58d0a5b3ea8da35fa3a76
Taiwo Osunrinde <55808005+osunrinde@users.noreply.github.com>
2023-11-30 22:55:41 -0700

    Update Freeport.py

--- Freeport.py
+++ Freeport.py
@@ -295,7 +295,7 @@ if authentication_status:
                                 OT37_patch, OT41_patch, OT42_patch, Overlap_patch])
 
 
-            st.write("Note: Interactive dashboard is displayed above.")
+            #st.write("Note: Interactive dashboard is displayed above.")
             #plt.show()
         else:
             cols_1 = [Ore_Type,LITH]
@@ -739,14 +739,20 @@ if authentication_status:
                        
                         #outlier execution button
                 with download_button:
-                    if st.button("Download Outliers"):
+                    if 'clicked' not in st.session_state:
+                        st.session_state.clicked = False
+
+                    def click_button():
+                        st.session_state.clicked = True
+                    
+                    if st.button("Download Outliers", on_click=click_button):
                         time.sleep(3)
                         download_time=datetime.now().strftime("%m%d%y_%H_%M_%S")
                         download_filename = f"Outliers_{download_time}.xlsx"
                         outliers_download(dataframes, download_filename)
                         with open(download_filename, "rb") as file:
                             st.download_button(label="download", data=file.read(), file_name=download_filename, key="download_button")
-                            st.success("Outliers successfully downloaded to your PC")
+                            #st.success("Outliers successfully downloaded to your PC")
                             st.write(f'there are {idx.sum():,} OT21 Outliers  of the {len(idx):,} holes')
                             st.write(f'there are {idx1.sum():,} OT22 Outliers  of the {len(idx1):,} holes')
                             st.write(f'there are {idx2.sum():,} OT27 Outliers  of the {len(idx2):,} holes')
@@ -756,10 +762,13 @@ if authentication_status:
                             st.write(f'there are {idx5.sum():,} OT37 Outliers of the {len(idx5):,} holes')
                             st.write(f'there are {idx6.sum():,} OT41 Outliers of the {len(idx6):,} holes')
                             st.write(f'there are {idx7.sum():,} OT42 Outliers of the {len(idx7):,} holes')
+                if st.session_state.clicked:
+                # The message and nested widget will remain on the page
+                    st.write("Outliers successfully downloaded to your PC")
                             
 
 
     if __name__ == "__main__":
         main()
 
-    
+   
