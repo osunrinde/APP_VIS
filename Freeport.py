c04b230820bd7d8be783faa1dd723d70438ed366
Taiwo Osunrinde <55808005+osunrinde@users.noreply.github.com>
2024-02-01 17:54:52 -0700

    Update Freeport.py

--- Freeport.py
+++ Freeport.py
@@ -341,12 +341,16 @@ elif st.session_state["authentication_status"]:
                         st.subheader("Filtered Assay Data:")
                         st.dataframe(data_plot)
                     else:
-                        data = data_c[~((data_c[st.session_state.ore_type] == 99) & (data_c['TCU'] >= 0))]
-                        data = data.loc[data[column_to_filter].str.startswith(tuple(filter_list))]
-                        data_plot=data.loc[data['TCU']>=0.1]
-                        data_plot = data_plot[~data_plot[st.session_state.ore_type].isin([10,50,51,52,53,54])]
-                        st.subheader("Filtered Assay Data:")
-                        st.dataframe(data_plot)
+                        selected_columns=data_c[column_to_filter]
+                        if selected_columns=='object'
+                            data = data_c[~((data_c[st.session_state.ore_type] == 99) & (data_c['TCU'] >= 0))]
+                            data = data.loc[data[selected_columns].str.startswith(tuple(filter_list))]
+                            data_plot=data.loc[data['TCU']>=0.1]
+                            data_plot = data_plot[~data_plot[st.session_state.ore_type].isin([10,50,51,52,53,54])]
+                            st.subheader("Filtered Assay Data:")
+                            st.dataframe(data_plot)
+                    else:
+                        st.warning('selected column cannot be used for filtering)
                         
     
                 
